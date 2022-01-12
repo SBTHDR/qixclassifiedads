835adb1f966b9b16d75950a7f4f1ba567e80a6ee
SBTHDR <subrata.halder13d@gmail.com>
2022-01-12 23:02:09 +0600

    Category Middleware

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -11,6 +11,9 @@ use App\Http\Requests\StoreCategoryRequest;
 
 class CategoryController extends Controller
 {
+    public function __construct() {
+        $this->middleware('auth');
+    }
     /**
      * Display a listing of the resource.
      *
