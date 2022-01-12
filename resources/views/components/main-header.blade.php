9f19616a55a32234797b4a10bd6f6a000a28efbe
SBTHDR <subrata.halder13d@gmail.com>
2022-01-12 19:41:19 +0600

    Auth UI

--- resources/views/components/main-header.blade.php
+++ resources/views/components/main-header.blade.php
@@ -13,13 +13,13 @@
         <a class="mr-5 hover:text-gray-900 cursor-pointer">Contact</a>
         @guest
         <a href="{{ route('login') }}" class="mr-5 hover:text-gray-900 cursor-pointer">Login</a>
-        <a class="mr-5 hover:text-gray-900 cursor-pointer">register</a>
+        <a href="{{ route('register') }}" class="mr-5 hover:text-gray-900 cursor-pointer">register</a>
         @endguest
         @auth
         <a href="{{ route('dashboard') }}" class="mr-5 hover:text-indigo-900 cursor-pointer text-indigo-500 font-bold">{{ Auth::user()->name }}</a>
         @endauth
       </nav>
-      <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">Add Listing
+      <button class="inline-flex items-center text-white bg-indigo-500 border-0 py-1 px-3 focus:outline-none hover:bg-indigo-700 rounded text-base mt-4 md:mt-0">Add Listing
         <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
           <path d="M5 12h14M12 5l7 7-7 7"></path>
         </svg>
