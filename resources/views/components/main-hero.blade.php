f4e0cfb88b59288c06a02baf9dc4d5445178c4bc
SBTHDR <subrata.halder13d@gmail.com>
2022-01-12 14:54:29 +0600

    Header Footer Content UI

---
+++ resources/views/components/main-hero.blade.php
@@ -0,0 +1,11 @@
+<header>
+    <div class="w-full bg-center bg-cover h-[32rem]" style="background-image: url(https://images.unsplash.com/photo-1606857521015-7f9fcf423740?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80);">
+        <div class="flex items-center justify-center w-full h-full bg-gray-900 bg-opacity-50">
+            <div class="text-center">
+                <h1 class="text-2xl font-semibold text-white uppercase lg:text-3xl">Create Add <span>Listing</span></h1>
+                <button class="w-full px-4 py-2 mt-4 text-sm font-medium text-white uppercase transition-colors duration-200 transform bg-indigo-500 rounded-md lg:w-auto hover:bg-indigo-400 focus:outline-none focus:bg-indigo-400">Listings</button>
+            </div>
+        </div>
+    </div>
+</header>
+        
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,4 +2,11 @@
     <x-slot name="header">
         <x-main-header></x-main-header>
     </x-slot>
+    <section>
+        <x-main-hero></x-main-hero>
+    </section>
+    <section>
+        <x-main-section></x-main-section>
+    </section>
+    <x-main-footer></x-main-footer>
 </x-main-layout>
