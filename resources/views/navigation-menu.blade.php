86048be6794db47853e237fcedd7ddbe5914b39c
SBTHDR <subrata.halder13d@gmail.com>
2022-01-12 20:19:28 +0600

    Migrations

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -18,6 +18,12 @@
                 </div>
 
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                    <x-jet-nav-link href="{{ route('categories.index') }}" :active="request()->routeIs('categories.index')">
+                        {{ __('Categories') }}
+                    </x-jet-nav-link>
+                </div>
+
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
                     <x-jet-nav-link href="/" :active="request()->routeIs('/')">
                         {{ __('Listing Site') }}
                     </x-jet-nav-link>
