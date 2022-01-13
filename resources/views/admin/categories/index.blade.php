256fa758af3ba204ff4d03a41b0f35f67774d546
SBTHDR <subrata.halder13d@gmail.com>
2022-01-13 15:04:21 +0600

    fix

--- resources/views/admin/categories/index.blade.php
+++ resources/views/admin/categories/index.blade.php
@@ -68,14 +68,12 @@
                                                 <a href="{{ route('categories.edit', $category->id) }}"
                                                     class="text-white px-4 py-2 bg-indigo-500 rounded">Edit</a>
                                                 <form action="{{ route('categories.destroy', $category->id) }}"
-                                                    method="POST">
+                                                    method="POST" onsubmit="return confirm('Are You Sure to delete?')">
                                                     @csrf
                                                     @method('DELETE')
-                                                    <a href="{{ route('categories.destroy', $category->id) }}"
-                                                        class="text-white px-4 py-2 bg-red-400 rounded" onclick="event.preventDefault();
-                                                            this.closest('form').submit();">
+                                                    <button class="text-white px-4 py-2 bg-red-400 rounded">
                                                         {{ __('Delete') }}
-                                                    </a>
+                                                    </button>
                                                 </form>
                                             </td>
                                         </tr>
