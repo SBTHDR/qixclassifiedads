de4ffa3f177f2526926ca492a6e3f024aa7abe0c
SBTHDR <subrata.halder13d@gmail.com>
2022-01-13 20:10:40 +0600

    Dashboard UI

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -23,11 +23,6 @@
                     </x-jet-nav-link>
                 </div>
 
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-jet-nav-link href="/" :active="request()->routeIs('/')">
-                        {{ __('Listing Site') }}
-                    </x-jet-nav-link>
-                </div>
             </div>
 
             <div class="hidden sm:flex sm:items-center sm:ml-6">
