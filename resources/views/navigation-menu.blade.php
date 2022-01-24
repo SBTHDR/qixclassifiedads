571abd0f7b3017c59626fd46e580d2d98ea8f236
SBTHDR <subrata.halder13d@gmail.com>
2022-01-24 23:08:00 +0600

    Dashboard Dropdown

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -35,6 +35,50 @@
                     </x-jet-nav-link>
                 </div>
 
+                <div class="hidden sm:flex sm:items-center sm:ml-6">
+                    <div class="ml-3 relative">
+                        <x-jet-dropdown align="right" width="48">
+                            <x-slot name="trigger">
+                                    <span class="inline-flex rounded-md">
+                                        <button type="button" class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition">
+                                            {{ __('Manage Region') }}
+
+                                            <svg class="ml-2 -mr-0.5 h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                                                <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                                            </svg>
+                                        </button>
+                                    </span>
+                            </x-slot>
+
+                            <x-slot name="content">
+                                <!-- Account Management -->
+                                <div class="block px-4 py-2 text-xs text-gray-400">
+                                    {{ __('Manage Account') }}
+                                </div>
+
+                                <x-jet-dropdown-link href="{{ route('profile.show') }}">
+                                    {{ __('Country') }}
+                                </x-jet-dropdown-link>
+
+                                <div class="border-t border-gray-100"></div>
+
+                                <x-jet-dropdown-link href="{{ route('profile.show') }}">
+                                    {{ __('State') }}
+                                </x-jet-dropdown-link>
+
+                                <div class="border-t border-gray-100"></div>
+
+                                <x-jet-dropdown-link href="{{ route('profile.show') }}">
+                                    {{ __('City') }}
+                                </x-jet-dropdown-link>
+
+                            </x-slot>
+                        </x-jet-dropdown>
+                    </div>
+                </div>
+
+                
+
             </div>
 
             <div class="hidden sm:flex sm:items-center sm:ml-6">
