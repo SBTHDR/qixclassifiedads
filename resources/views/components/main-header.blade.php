9e684f1fbe6e7e4d941c832b4658b9d75f56aec3
SBTHDR <subrata.halder13d@gmail.com>
2022-01-26 17:11:39 +0600

    Frontend Listings

--- resources/views/components/main-header.blade.php
+++ resources/views/components/main-header.blade.php
@@ -19,10 +19,10 @@
         <a href="{{ route('dashboard') }}" class="mr-5 hover:text-indigo-900 cursor-pointer text-indigo-500 font-bold">{{ Auth::user()->name }}</a>
         @endauth
       </nav>
-      <button class="inline-flex items-center text-white bg-indigo-500 border-0 py-1 px-3 focus:outline-none hover:bg-indigo-700 rounded text-base mt-4 md:mt-0">Add Listing
+      <a href="{{ route('listings.create') }}" class="inline-flex items-center text-white bg-indigo-500 border-0 py-1 px-3 focus:outline-none hover:bg-indigo-700 rounded text-base mt-4 md:mt-0">Add Listing
         <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
           <path d="M5 12h14M12 5l7 7-7 7"></path>
         </svg>
-      </button>
+      </a>
     </div>
   </header>
--- routes/web.php
+++ routes/web.php
@@ -28,16 +28,17 @@ Route::get('/all-listings', [FrontendListingController::class, 'index'])->name('
 
 Route::middleware(['auth:sanctum', 'verified'])->group(function () {
     Route::get('/dashboard', [AdminController::class, 'index'])->name('dashboard');
+    Route::resource('/listings', ListingController::class);
 });
 
-Route::resource('/categories', CategoryController::class);
-Route::resource('/subcategories', SubCategoryController::class);
-Route::resource('/childcategories', ChildCategoryController::class);
-
-Route::resource('/countries', CountryController::class);
-Route::get('/countries/{country}/add-state', [CountryController::class, 'add_state'])->name('add_state');
-Route::post('/countries/{country}/add-state', [CountryController::class, 'add_state_store'])->name('add_state.store');
-Route::resource('/states', StateController::class);
-Route::resource('/cities', CityController::class);
-
-Route::resource('/listings', ListingController::class);
+Route::middleware(['auth:sanctum', 'verified'])->group(function () {
+    Route::resource('/categories', CategoryController::class);
+    Route::resource('/subcategories', SubCategoryController::class);
+    Route::resource('/childcategories', ChildCategoryController::class);
+    
+    Route::resource('/countries', CountryController::class);
+    Route::get('/countries/{country}/add-state', [CountryController::class, 'add_state'])->name('add_state');
+    Route::post('/countries/{country}/add-state', [CountryController::class, 'add_state_store'])->name('add_state.store');
+    Route::resource('/states', StateController::class);
+    Route::resource('/cities', CityController::class);
+});
